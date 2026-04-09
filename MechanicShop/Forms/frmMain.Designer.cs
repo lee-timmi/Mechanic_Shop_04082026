c0b980015b41d8d250c1eceeba8e8446d406d6b7
thimmyle <ThaiThimmy2003@gmail.com>
2026-04-09 15:38:47 -0400

    feat(ui): add MainForm(FINISHED design) and RepairOrder Form.

--- MechanicShop/Forms/frmMain.Designer.cs
+++ MechanicShop/Forms/frmMain.Designer.cs
@@ -28,21 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button7 = new System.Windows.Forms.Button();
+            this.pboLogo = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gboOptions = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pboLogo)).BeginInit();
+            this.gboOptions.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(614, 431);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 35);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // pboLogo
+            // 
+            this.pboLogo.Location = new System.Drawing.Point(186, 12);
+            this.pboLogo.Name = "pboLogo";
+            this.pboLogo.Size = new System.Drawing.Size(390, 170);
+            this.pboLogo.TabIndex = 7;
+            this.pboLogo.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 446);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            // 
+            // gboOptions
+            // 
+            this.gboOptions.BackColor = System.Drawing.Color.Transparent;
+            this.gboOptions.Controls.Add(this.button6);
+            this.gboOptions.Controls.Add(this.button5);
+            this.gboOptions.Controls.Add(this.button4);
+            this.gboOptions.Controls.Add(this.button3);
+            this.gboOptions.Controls.Add(this.button2);
+            this.gboOptions.Controls.Add(this.button1);
+            this.gboOptions.Location = new System.Drawing.Point(63, 188);
+            this.gboOptions.Name = "gboOptions";
+            this.gboOptions.Size = new System.Drawing.Size(635, 230);
+            this.gboOptions.TabIndex = 9;
+            this.gboOptions.TabStop = false;
+            this.gboOptions.Text = "Options";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(436, 132);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(180, 80);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(219, 132);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(180, 80);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 132);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(180, 80);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(436, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(180, 80);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(219, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 80);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 80);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(568, 450);
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.BackgroundImage = global::MechanicShop.Properties.Resources.mechanicWorkshop1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(766, 491);
+            this.Controls.Add(this.gboOptions);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pboLogo);
+            this.Controls.Add(this.button7);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Main Form";
+            this.Text = "MECHANIC SHOP MANAGEMENT SYSTEM";
+            ((System.ComponentModel.ISupportInitialize)(this.pboLogo)).EndInit();
+            this.gboOptions.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox pboLogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gboOptions;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
---
+++ MechanicShop/Forms/frmRepairOrder.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace MechanicShop.Forms
+{
+    partial class frmRepairOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(120, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 28);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(120, 60);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(200, 28);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(120, 90);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(500, 90);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(120, 28);
+            this.comboBox3.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(120, 167);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(500, 265);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Labor";
+            // 
+            // frmRepairOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(878, 644);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "frmRepairOrder";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mechanic Shop - REPAIR ORDER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
